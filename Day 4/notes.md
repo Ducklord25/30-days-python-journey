117d10851b88ee23dcde486ae4a10f6c50961530
Aditya Mishra <adityamishra9194@gmail.com>
2026-03-28 02:09:19 +0530

    Day-4: almost completed

--- Day 4/notes.md
+++ Day 4/notes.md
@@ -22,4 +22,16 @@ i slept really late yesterday and woke up late today, so i feel a little letharg
     - %.2f - 2 digits after decimal
 - new style (use {} in string followed by .format(x,y,z...))
 - string interpolation ( use f"string {}" )
-    
+
+### Python String Operations
+- Strings are like ordered sequences like lists and tuples.
+- lang = 'python' is same as ['p','y','t','h','o','n'] list
+- many different string methods mentioned in practice.py
+
+# My thoughts
+- although i didnt do the exercise yet, and i didnt complete Day-4 on time. i am proud that i still showed up and tried to complete my work. 
+    - it is 2:07am right now and im doing my day-4
+- i feel tired, i think i will sleep and complete day-4 exercise and day-5 together after waking up
+- i am good, i am great, i am literally the best in whatever i do.
+- i am multitalented and love having fun in my own ways.
+- goodnight
--- Day 4/practice.py
+++ Day 4/practice.py
@@ -43,3 +43,58 @@ print('{} ** {} = {}'.format(a, b, a ** b))
 # string interpolation (fav method)
 
 print(f"the area of the circle with radius {radius} is {area_of_circle}")
+
+# STRING OPERATIONS !!!!!!!!!!!!!!!!1
+
+language = 'python'
+print(language[0]) # p
+print(language[1]) # y
+print(language[2]) # t
+print(language[3]) # h
+print(language[4]) # o
+print(language[5], 'end') # n
+
+print(language[-1]) # n reverse order
+print(language[-2]) # o
+print(language[-3]) # h
+print(language[-4]) # y
+
+# String Slicing
+
+print(language[0:3]) # pyt starts at 0 index and goes until index 3 EXCLUSIVELY
+print(language[3:9]) # hon even if you exceed upper limit ie, 5, it will print result
+print(language[:3]) # pyt
+print(language[-3:-3]) # prints blank
+print(language[3:])
+
+''' basically the first value indicated where to start, second value indicates
+where to stop and the third value tells the compiler what step value to take'''
+
+a = "my name is aditya"
+print(a[2::1]) #starts at index 2, second value infinite, third value is default step
+print(a[0::3]) #starts at index 0, second value infinite, third value is skip 2
+#if skip value is 3, it will skip 2 digits only
+
+print(a.capitalize()) # first letter caps
+print(a.count('a', 0, 17)) # count of (substring, start= , end= )
+print(a.endswith('ya')) # true
+print(a.endswith('b')) # false
+print(a.expandtabs(100)) # replaces tab size with spaces, default size is 8
+print(a.find("a")) # finds the first occurence and returns index, not found -1
+print(a.rfind('a')) # same but last occurence
+print(a.isalnum()) # checks if alphanumeric, whitespace is not alnum
+print(a.isalpha()) # checks if all elements belong to a-z, A-Z, space is not included
+print(a.isdecimal())
+print(a.isdigit())
+print(a.isnumeric())
+print(a.islower())
+print(a.isupper())
+
+hy = ['aditya', 'naughty', 'eshika', 'smarty']
+print(' '.join(hy)) # aditya naughty eshika smarty
+print('$$'.join(hy)) # aditya$$naughty$$eshika$$smarty
+print(a.strip('y')) # removes these letters only from start and end
+print(a.replace('aditya','eshika')) # replaces first substring with second
+print(a.split()) 
+print(a.title())
+print(a.swapcase())
